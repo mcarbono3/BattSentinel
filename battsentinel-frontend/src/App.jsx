c5589abcc6fbbdf70b4397aba8a5996fa31fab19
mcarbono3 <ing.mariocarbono@gmail.com>
2025-06-22 16:58:17 -0500

    App.jsx

--- battsentinel-frontend/src/App.jsx
+++ battsentinel-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/components/theme-provider'
 import { AuthProvider, useAuth } from '@/contexts/AuthContext'
@@ -163,4 +163,3 @@ function App() {
 }
 
 export default App
-
