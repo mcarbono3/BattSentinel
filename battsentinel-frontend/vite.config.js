0eea29f5573f63362883755d6dd1785996fcd7c5
mcarbono3 <ing.mariocarbono@gmail.com>
2025-06-22 19:33:26 -0500

    vite.config.js

--- battsentinel-frontend/vite.config.js
+++ battsentinel-frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/BattSentinel/',
+  base: '/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
