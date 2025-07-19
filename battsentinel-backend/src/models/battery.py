3d945a0111055f80cf8358945b700035a9c88060
mcarbono3 <ing.mariocarbono@gmail.com>
2025-07-19 06:42:25 -0500

    battery.py

--- battsentinel-backend/src/models/battery.py
+++ battsentinel-backend/src/models/battery.py
@@ -85,8 +85,7 @@ class Battery(db.Model):
             data['temperature'] = None
             data['is_plugged'] = None
             data['latest_timestamp'] = None
-        # 3. CORRECCIÓN CRÍTICA: Campo current_soh para el frontend
-        data['current_soh'] = data['soh']  # Alias para compatibilidad con frontend
+
         # 4. Finalmente, retornar el diccionario 'data' completo
         return data
 
